37d14acdde26be00302890d6dc59db19917750b1
Bhargav Panth <bhargavrpanth@gmail.com>
2017-01-13 18:46:37 +0530

    question controller fetches questions based on skills and difficulty

--- API/server/controllers/QuestionController.js
+++ API/server/controllers/QuestionController.js
@@ -13,4 +13,14 @@ var User = mongoose.model('User', UserSchema);
 var QuestionSchema = require('../models/Question.js');
 var Questions = mongoose.model('Question', QuestionSchema);
 
+module.exports = function(app, route) {
+	router.get('/', function(req, res){
+		// pass array of 
+	});
+	router.post('/', function(req, res){
+		// pass skill 
+	});
+	return router;
+}
+
  
